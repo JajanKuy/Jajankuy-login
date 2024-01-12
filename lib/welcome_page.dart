4539513828014ffce0964dc48ce9eb4751c25122
heenzu <farizirza77@gmail.com>
2024-01-12 21:45:09 +0700

    hackfest-stage2

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_super_parameters
 
 import 'package:firebase_app/auth_controller.dart';
 import 'package:flutter/material.dart';
